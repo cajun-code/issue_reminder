28633c26e00db04fbaf5aa19adc1b8677cf51227
Allan Davis <javaalley@gmail.com>
2010-12-27 14:25:03 -0500

    Finished front end for reminder administration

--- app/controllers/reminder_controller.rb
+++ app/controllers/reminder_controller.rb
@@ -3,8 +3,27 @@ class ReminderController < ApplicationController
 
 
   def create
+    reminder = Reminder.new params[:reminder]
+    reminder.save
+    populate_reminders reminder.issue_id    
+    render :action => "index"    
   end
 
   def delete
+    reminder = Reminder.find params[:id]
+    issue_id = reminder.issue_id
+    reminder.delete
+    populate_reminders issue_id    
+    render :action => "index"  
+  end
+  def index
+    populate_reminders params[:issue_id]
+    respond_to do |format|
+      format.js 
+    end
+  end
+  private
+  def populate_reminders (issue_id)
+    @reminders = Issue.find(issue_id).reminders
   end
 end
