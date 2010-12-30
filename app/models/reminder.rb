b799085d58653a17bac19135b979a688fd71bd57
Allan Davis <javaalley@gmail.com>
2010-12-30 16:56:34 -0500

    added validation to model

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,4 +1,12 @@
 class Reminder < ActiveRecord::Base
   unloadable
   belongs_to :issue
+  validate :valid_date?
+  
+  private
+  def valid_date?
+    unless reminder_date && reminder_date >= Date.today 
+      errors.add(:reminder_date, "Reminder date is invalid")
+    end
+  end
 end
