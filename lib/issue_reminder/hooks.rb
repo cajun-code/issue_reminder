3c63e4c96889b945c5cbd804d724025e3662b3d1
Allan Davis <javaalley@gmail.com>
2010-12-30 16:36:19 -0500

    added reminder date to new issue form

--- lib/issue_reminder/hooks.rb
+++ lib/issue_reminder/hooks.rb
@@ -1,21 +1,15 @@
 module IssueReminder
   class Hooks < Redmine::Hook::ViewListener
     ### Show on new and update forms
-    # render_on :view_issues_form_details_bottom, 
-    # :partial => "issue/issue_reminder"
-    # def view_issues_form_details_bottom(context={ })
-    #   context[:reminders]= context[:issue].reminders
-    #   context[:reminder] = Reminder.new
-    #   context[:controller].send(:render_to_string, {
-    #     :partial => "issue/issue_reminder",
-    #     :locals => context 
-    #   })
-    # end
-    # def controller_issues_new_before_save(context={})
-    #   context[:reminder]
-    # end
-    # alias_method :controller_issues_edit_before_save, :controller_issues_new_before_save
+    render_on :view_issues_form_details_bottom, 
+    :partial => "issue/issue_reminder_form"
     
+    
+    def controller_issues_new_after_save(context={ })      
+      reminder_date = context[:params][:reminder_date]
+      context[:issue].reminders.create(:reminder_date => reminder_date) unless reminder_date.blank?
+    end
+    # alias_method :controller_issue_edit_after_save, :controller_issues_new_after_save
     ### Show on Show form
     #:view_issues_show_description_bottom
     def view_issues_show_description_bottom(context={ })
